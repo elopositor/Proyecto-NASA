d5602f8e3a3b68071f60a3ecdbb271f060421498
Mane21 <44374569+Mane21@users.noreply.github.com>
2023-03-28 20:03:38 +0200

    [v.1.0.0] Updating crossvalidation script

--- CrossValidation.py
+++ CrossValidation.py
@@ -1,6 +1,16 @@
 # Double cross-validation
 import numpy as np
-import sklearn
+import pandas as pd
+from sklearn import svm
 
+# Creating data frame
+Xrand = np.random.randn(1000,40)
+X = pd.DataFrame(Xrand)
+X.head(10)
 
+Y = np.array([0, 1])
+
+# Filas: muestras / registros
+# Columnas: variables / features
+X.np.sp
 
