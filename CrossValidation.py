6ba9be019c8732ac5260e4a539a813b91359b8cb
Mane21 <44374569+Mane21@users.noreply.github.com>
2023-04-15 13:03:47 +0200

    [v.1.0.0] Cambios

--- CrossValidation.py
+++ CrossValidation.py
@@ -4,13 +4,12 @@ import pandas as pd
 from sklearn import svm
 
 # Creating data frame
-Xrand = np.random.randn(1000,40)
+Xrand = np.random.randn(1000, 40)
 X = pd.DataFrame(Xrand)
 X.head(10)
 
-Y = 12
+Y = np.array([0, 1])
 
 # Filas: muestras / registros
 # Columnas: variables / features
 X.np.sp
-
